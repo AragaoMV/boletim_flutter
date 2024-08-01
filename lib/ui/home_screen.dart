42a2701003558c01ca1dbad2ce18e001a861d325
AragaoMV <marcosvinisa@icloud.com>
2024-08-01 11:14:59 -0300

    criação botão add, personalização de cor pela nota

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -41,9 +41,9 @@ class _HomeScreenState extends State<HomeScreen> {
       context: context,
       builder: (context) {
         return AlertDialog(
-          title: Text(
-              materia == null ? "Adicionar Disciplina" : "Editar Disciplina"),
-          content: Column(children: [
+          title: Text(materia == null ? "Adicionar Disciplina" : "Editar Disciplina"),
+          content: Column(mainAxisSize: MainAxisSize.min,
+              children: [
             TextField(
               controller: _disciplinaController,
               decoration: const InputDecoration(hintText: "disciplina"),
@@ -51,6 +51,7 @@ class _HomeScreenState extends State<HomeScreen> {
             TextField(
               controller: _notaController,
               decoration: const InputDecoration(hintText: "nota"),
+              keyboardType: TextInputType.number,
             ),
           ]),
           actions: [
@@ -58,15 +59,14 @@ class _HomeScreenState extends State<HomeScreen> {
                 onPressed: () {
                   Navigator.pop(context);
                 },
-                child: const Text("Cancelar")),
+                child: const Text("Cancelar"),),
             ElevatedButton(
                 onPressed: () async {
                   final disciplina = _disciplinaController.text;
                   final nota = int.tryParse(_notaController.text);
-                  if (disciplina.isNotEmpty && nota == null) {
+                  if (disciplina.isNotEmpty && nota != null) {
                     if (materia == null) {
-                      await dbHelper.insertMateria(
-                          {"disciplina": disciplina, "nota": nota});
+                      await dbHelper.insertMateria({"disciplina": disciplina, "nota": nota});
                       _updateMateriaMapList();
                     } else {
                       await dbHelper.updateMateria({
@@ -79,12 +79,17 @@ class _HomeScreenState extends State<HomeScreen> {
                     Navigator.pop(context);
                   }
                 },
-                child: Text(materia == null ? "Adicionar" : "Atualizar"))
+                child: Text(materia == null ? "Adicionar" : "Atualizar"),)
           ],
         );
       },
     );
   }
+  _deleteMateria(int id)async{
+    await dbHelper.deleteMateria(id);
+    _updateMateriaMapList();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -93,9 +98,32 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text("Boletim de Notas"),
         centerTitle: true,
         titleTextStyle: const TextStyle(
-            color: Colors.black, fontSize: 24, fontWeight: FontWeight.w500),
-        backgroundColor: Color.fromARGB(97, 0, 30, 95),
+            color: Colors.white, fontSize: 24, fontWeight: FontWeight.w500),
+            backgroundColor: const Color.fromARGB(255, 95, 0, 160),
       ),
-    );
-  }
+    body: ListView.builder(
+      itemCount: materias.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(materias[index].disciplina),
+          subtitle: Text(materias[index].nota.toString()),
+          tileColor: materias[index].nota < 5? Colors. red : Colors.green,
+          trailing: SizedBox(width: 100, child: Row(children: [
+            IconButton(onPressed: () {
+              _showFormDialog(materias[index]);
+            }, icon: const Icon(Icons.edit)),
+            IconButton(onPressed: () {
+              _deleteMateria(materias[index].id);
+            }, icon: const Icon(Icons.delete))
+          ],),
+          ),
+        );
+      },
+    ),
+      floatingActionButton: FloatingActionButton(onPressed:() {
+      _showFormDialog(null);
+      },backgroundColor: const Color.fromARGB(255, 95, 0, 160),
+        child: const Icon(Icons.add),),
+  );
+ }
 }
