7412cef47aa90598ab8411544b654b203792c623
Marcos Aragão <70450278+AragaoMV@users.noreply.github.com>
2024-08-02 12:05:32 -0300

    Separação de disciplina e calculo de notas

--- lib/models/materia_model.dart
+++ lib/models/materia_model.dart
@@ -1,17 +1,34 @@
 class MateriaModel {
   final int id;
   final String disciplina;
-  final int nota;
+  final List<int> notas;
+  final bool isSemestral;
 
-  MateriaModel(
-      {required this.id, required this.disciplina, required this.nota});
+  MateriaModel({
+    required this.id,
+    required this.disciplina,
+    required this.notas,
+    required this.isSemestral,
+  });
 
   Map<String, dynamic> toMap() {
-    return {"id": id, "disciplina": disciplina, "nota": nota};
+    return {
+      'disciplina': disciplina,
+      'notas': notas.join(','),
+      'isSemestral': isSemestral ? 1 : 0,
+    };
   }
 
   factory MateriaModel.fromMap(Map<String, dynamic> map) {
     return MateriaModel(
-        id: map["id"], disciplina: map["disciplina"], nota: map["nota"]);
+      id: map['id'],
+      disciplina: map['disciplina'],
+      notas: (map['notas'] as String).split(',').map((e) => int.parse(e)).toList(),
+      isSemestral: map['isSemestral'] == 1,
+    );
+  }
+
+  double calcularMedia() {
+    return notas.isNotEmpty ? notas.reduce((a, b) => a + b) / notas.length : 0.0;
   }
 }
